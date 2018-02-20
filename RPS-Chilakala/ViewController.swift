d1a3b177a828c41dabfe62246bf12e88222fdb40
student <s530669@ol10032m.nwmissouri.edu>
2018-02-19 20:27:33 -0600

    Initial Commit

---
+++ RPS-Chilakala/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RPS-Chilakala
+//
+//  Created by Chilakala Krishna Reddy,Darshan Reddy on 2/19/18.
+//  Copyright © 2018 Chilakala Krishna Reddy,Darshan Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
